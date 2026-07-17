9978225bcc2f0d72fce57f2124f54c576cf1c037
aatif-21 <aatifsaman866@gmail.com>
2026-07-17 17:19:20 +0530

    Update string example

--- STRING/main.cpp
+++ STRING/main.cpp
@@ -1,3 +1,13 @@
-//
-// Created by aatif on 17-07-2026.
-//
+#include <iostream>
+using namespace std;
+int main() {
+    string name;
+    name.push_back('a');
+    name.push_back('b');
+    name.push_back('c');
+    cout << name << endl;
+    //string surname = "babbar";
+    //surname = "atif";
+    //cout << surname << endl;
+    return 0;
+}

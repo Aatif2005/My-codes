e19653ce7b03b49c707e732439fe9d730f1b8cf2
aatif-21 <aatifsaman866@gmail.com>
2026-07-18 13:37:02 +0530

    Add reverse string program

---
+++ STRING/reverse_string.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+string reverseString(string str) {
+    // now using loops
+    int len = str.length();
+    string reversed = "";
+    for (int i = len-1; i >= 0; i--) {
+        reversed = reversed + str[i];
+    }
+    //reverse(str.begin(), str.end());
+    //return str;
+    return reversed;
+}
+int main() {
+    //string str = "Atif";
+    //cout << reverseString(str) << endl;
+    string str;
+    cout << "Enter the string to be reversed: " << endl;
+    cin >> str;
+    cout << reverseString(str) << endl;
+    return 0;
+}

db9e9a692b9d97b4895dd653cf0a3aad9635e96a
aatif-21 <abhixtoxicc@gmail.com>
2026-08-01 21:09:28 +0530

    Initial commit

--- LEET CODE/Segregate_0s_1s .cpp
+++ LEET CODE/Segregate_0s_1s .cpp
@@ -7,24 +7,37 @@ Output: [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]
 Explanation:  After segregation, all the 0's are on the left and 1's are on the right. Modified array will
 be [0, 0, 0, 0, 0, 1, 1, 1, 1, 1].*/
 
-class Solution {
-public:
-    void segregate0and1(vector<int> &arr) {
-        int n = arr.size();
-        int left = 0;
-        int right = n-1;
-        while(left < right) {
-            if(arr[left] == 0) {
-                left++;
-            }
-            else if(arr[right] == 1) {
-                right--;
-            }
-            else {
-                swap(arr[left], arr[right]);
-                left++;
-                right--;
-            }
+#include <iostream>
+using namespace std;
+
+void segregate0and1(int arr[], int n) {
+    int left = 0;
+    int right = n - 1;
+
+    while (left < right) {
+        if (arr[left] == 0) {
+            left++;
+        }
+        else if (arr[right] == 1) {
+            right--;
+        }
+        else {
+            swap(arr[left], arr[right]);
+            left++;
+            right--;
         }
     }
-};
+}
+
+int main() {
+    int arr[] = {0, 1, 0, 1, 1, 0, 0, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    segregate0and1(arr, n);
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
